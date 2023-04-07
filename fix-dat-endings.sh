8763ae72774b29d00fd22044ab61cade47af0a49
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-04-07 19:58:39 -0400

    In case Simutranslator still needs this (it probably doesn't), auto-fix dat file terminators

---
+++ fix-dat-endings.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# This script goes through the current directory,
+# finds all the .dat files,
+# and checks their last lines.
+#
+# If the last line is all whitespace, it's deleted (repeatedly)
+#
+# Then if the last line doesn't start with "--", "--" is added.
+#
+# This is designed to make the .dat files Simutranslator-compliant.
+
+FILES=`find . -name "*.dat" -print`
+# Note, crucial to not use the quotes around $FILES, so that the file list gets split
+for x in $FILES; do
+    TAIL=`tail -1 "$x"`
+    # Bash regular expression
+    if [[ "$TAIL" =~ --.* ]]; then
+        # echo "$x: OK"
+    else
+        # Append to file
+        echo "--" >> $x
+        echo "$x: fixed"
+    fi
+done
