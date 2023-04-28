d75609c24ab87f8102fc0c09e12064eab8be8cf5
Nathanael Nerode <ncn_mail10@fastmail.fm>
2023-04-28 13:16:43 -0400

    fix-dat-endings.sh: version which removes trailing whitespace before adding terminators

--- fix-dat-endings.sh
+++ fix-dat-endings.sh
@@ -9,6 +9,14 @@
 # This is designed to make the .dat files Simutranslator-compliant.
 
 FILES=`find . -name "*.dat" -print`
+# First pass: remove trailing whitespace
+# This rather unreadable sed program will do this.
+# See https://stackoverflow.com/questions/7359527/removing-trailing-starting-newlines-with-sed-awk-tr-and-friends
+for x in $FILES; do
+    sed -n '/^[[:space:]]*$/ !{x;/\n/{s/^\n//;p;s/.*//;};x;p;}; /^[[:space:]]*$/H' -i $x
+done
+echo "Trailing whitespaces removed"
+
 # Note, crucial to not use the quotes around $FILES, so that the file list gets split
 for x in $FILES; do
     TAIL=`tail -1 "$x"`
@@ -18,6 +26,6 @@ for x in $FILES; do
     else
         # Append to file
         echo "--" >> $x
-        echo "$x: fixed"
+        echo "$x: terminator added"
     fi
 done
